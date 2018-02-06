09ba0753284932443e7a207f702a41a6cd714238
T.Hrytsaienko <kortatiana89@gmail.com>
2018-02-06 13:04:47 +0200

    Facebook posts added

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
+// this.addEventListener('install', function (event) {
+//     event.waitUntil(
+//         caches.open('v1').then(function (cache) {
+//             return cache.addAll([
+//                 '/',
+//                 '/gallery/1.jpg',
+//                 '/gallery/2.jpg',
+//                 '/gallery/3.jpg',
+//                 '/style.css',
+//                 '/index.html',
+//                 '/index.js',
+//                 '/app.js'
+//             ]);
+//         })
+//     );
+// });
+
+// this.addEventListener('fetch', function (event) {
+//     var response;
+//     event.respondWith(
+//         caches.match(event.request).catch(function () {
+//             return fetch(event.request);
+//         }).then(function (r) {
+//             response = r;
+//             caches.open('v1').then(function (cache) {
+//                 cache.put(event.request, response);
+//             });
+//             return response.clone();
+//         }).catch(function () {
+//             return caches.match('/gallery/1.jpg');
+//         })
+//     );
+// });
+
+
+
 var CACHE_NAME = 'dependencies-cache';
 
 var REQUIRED_FILES = [
